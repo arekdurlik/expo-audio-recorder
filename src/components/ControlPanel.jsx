a462a1df581c53937f9f88a293628b42fa093574
arekdurlik <72689308+arekdurlik@users.noreply.github.com>
2022-06-07 17:34:46 +0200

    unnecessary check

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -62,7 +62,7 @@ const ControlPanel = () => {
         recording.setOnRecordingStatusUpdate(({ durationMillis, metering }) => {
           const loudness = metering + 100
           if (typeof loudness === 'number' && !Number.isNaN(loudness)) {
-            if (recordingData.current.length >= 18) recordingData.current.shift()
+            recordingData.current.shift()
             recordingData.current.push(clamp(loudness, 0 , 100))
           }
           
